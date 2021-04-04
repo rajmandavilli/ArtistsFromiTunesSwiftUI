f2cbd5f3f519994e3010de895fcc55b52b5e4a1a
rajmandavilli <81313194+rajmandavilli@users.noreply.github.com>
2021-04-03 22:42:37 -0500

    Initial Commit

---
+++ ArtistsFromiTunesSwiftUI/ArtistsFromiTunesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistsFromiTunesSwiftUIApp.swift
+//  ArtistsFromiTunesSwiftUI
+//
+//  Created by Raj Mandavilli on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct ArtistsFromiTunesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
